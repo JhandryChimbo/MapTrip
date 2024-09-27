40900b6d6bd805a32cf9385479477f5e6c767885
JhandryChimbo <jhandrychimbo@gmail.com>
2024-09-27 12:20:53 -0500

    Add Method to Auth

--- back_end/routes/maptrip.js
+++ back_end/routes/maptrip.js
@@ -17,8 +17,60 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/listar', LugarControl.listar_lugares);
-router.get('/listar_tipo', LugarControl.listar_lugares_tipo);
+const auth = function middleware() {
+  return async function (req, res, next) {
+    const token = req.headers["maptrip-token"];
+
+    if (token === undefined) {
+      res.status(401);
+      res.json({
+        msg: "ERROR",
+        tag: "Falta token",
+        code: 401,
+      });
+    } else {
+      require("dotenv").config();
+      const key = process.env.KEY_JWT;
+      jwt.verify(token, key, async (err, decoded) => {
+        if (err) {
+          res.status(401);
+          res.json({
+            msg: "ERROR",
+            tag: "Token no valido o expirado",
+            code: 401,
+          });
+        } else {
+          console.log(decoded.external);
+          const models = require("../app/models");
+          const cuenta = models.cuenta;
+          const aux = await cuenta.findOne({
+            where: { external_id: decoded.external },
+            include: [
+              {
+                model: models.personal,
+                as: "personal",
+                attributes: ["apellidos", "nombres"],
+              },
+            ],
+          });
+          if (aux === null) {
+            res.status(401);
+            res.json({
+              msg: "ERROR",
+              tag: "Token no valido",
+              code: 401,
+            });
+          } else {
+            next();
+          }
+        }
+      });
+    }
+  };
+};
+
+router.get('/listar',LugarControl.listar_lugares);
+router.get('/listar_tipo', auth(), LugarControl.listar_lugares_tipo);
 
 router.post('/guardar', PersonalControl.guardar_personal);
 
